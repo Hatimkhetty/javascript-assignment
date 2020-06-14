8dd30ec43064bdf0f135fd67f7777757e4fb5c46
hatimkhetty <hatimfkhetty53@gmail.com>
2020-06-14 20:24:16 +0500

    chapter 3 done

---
+++ java.3/apss.js
@@ -0,0 +1,22 @@
+// chapter 3 //
+
+// task 1 //
+
+var age = "I am 20 years old";
+alert("age")
+
+// task 2  //
+var number_of_time_website_visited = 14;
+alert("you have visited this site" + " " + number_of_time_website_visited + " " + "times");
+
+// task 3 //
+var birth_year = 2000;
+var type = typeof(birth_year);
+document.write("My birth year is " + birth_year + "</br>");
+document.write("Data type of my decleared variable is " + type);
+
+//task 4 //
+var name = "John Doe";
+var product_title = "T-Shirt";
+var quantity = 5;
+document.write(name + " orderd" + " " + quantity + " " + product_title + " " + "(s)on XYZ Clothing store");
