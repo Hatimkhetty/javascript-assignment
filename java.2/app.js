e252b720d00aa22ae709ec7d2a8b94fab0897faa
hatimkhetty <hatimfkhetty53@gmail.com>
2020-06-14 20:09:53 +0500

    java-assignment

---
+++ java.2/app.js
@@ -0,0 +1,26 @@
+// chapter 2 //
+// tasks 1 //
+var username;
+// tasks 2 //
+var myname = "Hatim Fahkrudiin Khetty";
+// tasks 3 //
+var massage = "hello world";
+alert(massage)
+    // tasks 4 //
+var name = "hatim";
+alert(name)
+var age = "20 year old";
+alert(age);
+var certificate = "web & mobile app";
+alert(certificate);
+// task 5 //
+var food = "pizza \n pizz \n piz \n pi \n p"
+alert(food);
+// tasks 6 //
+var email = "hf.khetty@gmail.com"
+alert(email);
+// tasks 7 //
+var book = " ``A smarter Way To Learn Javascript``"
+alert(book)
+    // tasks 8 //
+document.write("Yah! I Can write HTML content through Javascript")
---
+++ java/app.js
@@ -0,0 +1,10 @@
+ alert("Error!please enter a valid password");
+
+
+ alert("Welcome to Js land.. \n Happy Coding")
+
+ alert("welcome to js land..")
+
+ alert("Happy coding  \n ⬜ Prevent this page from creating additional dialouge")
+
+ alert("Hello.. I can ru JS through my web browser's console")
