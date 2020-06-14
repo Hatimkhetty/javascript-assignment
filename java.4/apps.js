302230969ada2df8bffad1dc403a63ae83aa8676
hatimkhetty <hatimfkhetty53@gmail.com>
2020-06-14 20:35:53 +0500

    chapter 4 done

---
+++ java.4/apps.js
@@ -0,0 +1,30 @@
+// chapter 4 // 
+
+// task 1  //
+var elephant, mouse, ant;
+
+// task 2 //
+//  vlegal variable. //
+var one;
+var some_thing;
+var helloWorld;
+var check123;
+var Country;
+
+// illegal variable //
+// var @user;
+// var 123 check;
+var _something;
+var something;
+var helloWorld;
+
+// task 3 //
+document.write("<h1>Rules for naming js variable </h1>");
+document.write("</br>");
+document.write("variable name can only contain, numbers, $ ,and,_. For example : $my_1stVaraible");
+document.write("</br>");
+document.write("variable must begin with a letter, $ or _. For example : $name, _name or name");
+document.write("</br>");
+document.write("variable names are case sensitive");
+document.write("</br>");
+document.write("variable name should not be JS keyword");
